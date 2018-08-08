868fbe2d0bfb5382a11f13c75848f3d6157aabda
Dylan <dylan.lawless@protonmail.com>
2018-08-08 14:26:19 +0200

    Annotating the commands

--- websearch.sh
+++ websearch.sh
@@ -1,7 +1,16 @@
+# Ask the user for the file or path to the list of terms to websearh
 read -e -p "Enter search list, use tab for completion: " file
+
+# Read the list and print out for the user to see
 echo "\nNow searching"
 cat "$file"
 
+# The system default web browser will need to be opened if it is not already running.
+# I used Safari here, this could be changed to automatically open the default
+open -a safari
+
+# Open a browser tab and search for each term line in the list
+# Lists greater than 10 terms should be split (i.e. $ split -l 10 list.txt)
 filename="$file"
 while IFS= read -r line;do
 echo $line
