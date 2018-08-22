acd1ac080631b2622281cf70b18428829fa14fa3
Dylan Lawless <lawless@svdhcp-1-040.epfl.ch>
2018-08-22 16:04:24 +0200

    Method to provide different output styles.

--- extract_columns.sh
+++ extract_columns.sh
@@ -14,3 +14,9 @@
 
 # Tab delim
 awk 'FNR==1{f++}{a[f,FNR]=$3}END{for(x=1;x<=FNR;x++){for(y=1;y<ARGC;y++)printf("%s      ",a[y,x]);print ""}}' ./../*/pheno.txt > master.txt
+
+# The spacer method can be changed; tab, space, comma, etc.
+# Another way to convert later is with the following command.
+# [The tab character (after ``s/'' in the following command) must be removed and printed to the command line using ``ctrl+v'' then ``tab''.]
+
+# \medskip \texttt{\$ sed 's/ /,/g' input.tsv > output.csv}
