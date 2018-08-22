0fdb1e149d6b856bbefef24e2a5fb26be172f1f9
Dylan Lawless <lawless@svdhcp-1-040.epfl.ch>
2018-08-22 15:49:35 +0200

    Update extract_columns.sh to used tab delim output.

--- extract_columns.sh
+++ extract_columns.sh
@@ -10,7 +10,7 @@
 
 # This takes column 3 from every file and appends to the output file
 # Space delim
-awk 'FNR==1{f++}{a[f,FNR]=$3}END{for(x=1;x<=FNR;x++){for(y=1;y<ARGC;y++)printf("%s ",a[y,x]);print ""}}' ./*_bin_clin/pheno.txt
+# awk 'FNR==1{f++}{a[f,FNR]=$3}END{for(x=1;x<=FNR;x++){for(y=1;y<ARGC;y++)printf("%s ",a[y,x]);print ""}}' ./../*/pheno.txt
 
 # Tab delim
-# awk 'FNR==1{f++}{a[f,FNR]=$3}END{for(x=1;x<=FNR;x++){for(y=1;y<ARGC;y++)printf("%s      ",a[y,x]);print ""}}' ./*/pheno.txt > master.txt
+awk 'FNR==1{f++}{a[f,FNR]=$3}END{for(x=1;x<=FNR;x++){for(y=1;y<ARGC;y++)printf("%s      ",a[y,x]);print ""}}' ./../*/pheno.txt > master.txt
